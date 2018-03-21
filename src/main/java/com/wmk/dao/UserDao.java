21715acc0d7008f5bf40a6460320a664a2bf17f7
linxidream <362499773@qq.com>
2018-03-21 15:39:03 +0800

    mybatis done

--- src/main/java/com/wmk/dao/UserDao.java
+++ src/main/java/com/wmk/dao/UserDao.java
@@ -1,9 +1,7 @@
 package com.wmk.dao;
 
 import com.wmk.entity.User;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
